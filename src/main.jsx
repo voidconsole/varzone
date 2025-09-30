0ecc764cbf613c57a2f7e9395be484d568694a25
Satwik Bhusanur <bhusanursatwik@gmail.com>
2025-09-30 22:58:11 +0530

    fixed styling issues

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router } from "react-router-dom"
+import './index.css'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/varzone">
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+createRoot(document.getElementById("root")).render(
+        <Router basename="/varzone">
+            <App />
+        </Router>
+
 )
